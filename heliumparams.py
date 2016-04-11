0e6b44014964c02e4e0f21c8fb8cbc498fcdbc2e
idrach55 <isaac@drachman.org>
2016-04-11 09:24:05 -0400

    PnL Plotter

--- heliumparams.py
+++ heliumparams.py
@@ -3,9 +3,9 @@ params = {
     'debug'             : True,
     'dump_on_lockdown'  : False,
     'max_distance'      : 0.75, 
-    'spread'            : 0.08, 
-    'trade_size'        : 1.00, 
+    'spread'            : 0.10, 
+    'trade_size'        : 0.50, 
     'stop_loss'         : 0.30,
     'max_inactive_time' : 60,
-    "shading"		    : 0.95,
+    "shading"		    : 0.75,
 }
