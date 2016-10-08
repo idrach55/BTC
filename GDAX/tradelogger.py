4384b811f5f7efa0705292fa9c1da743c8b880e3
idrach55 <isaac@drachman.org>
2016-10-08 15:05:19 -0400

    Added tradelogger
    Glosten now model that hooks into trade logger

---
+++ GDAX/tradelogger.py
@@ -0,0 +1,105 @@
+from autobahn.twisted.websocket import WebSocketClientFactory, connectWS
+from twisted.python import log
+from twisted.internet import reactor
+from blobprotocol import BlobProtocol
+from pprint import pprint
+from book import Book, BookClient
+from datetime import datetime
+
+import pandas as pd
+import sys
+import numpy as np
+import scipy.stats as dists
+
+
+class Glosten():
+    def __init__(self, xk):
+        self.name = 'glosten'
+        self.sigma, self.alpha, self.eta = xk
+
+    def setup(self, mid):
+        self.v0 = 100*mid
+        self.vals = (self.v0 - 4*100*self.sigma + np.arange(int(8*100*self.sigma) + 1))/100.
+        self.prob = dists.norm.pdf(100*self.vals, loc=self.v0, scale=100*self.sigma)
+
+    def out(self, side, price, size):
+        modup = (1 - self.alpha)*self.eta + self.alpha
+        moddn = (1 - self.alpha) - (1 - self.alpha)*self.eta
+
+        idx = int(100*price + 400*self.sigma - self.v0)
+        if side == 'buy':
+            self.prob[idx+1:] *= modup
+            self.prob[:idx+1] *= moddn
+        elif side == 'sell':
+            self.prob[:idx] *= modup
+            self.prob[idx:] *= moddn
+        self.prob /= self.prob.sum()
+
+        return (self.vals*self.prob).sum()
+
+
+class TradeLogger(BookClient):
+    def __init__(self, model=None):
+        self.fname = 'data/'+datetime.now().strftime('%Y-%m-%d')+'.'
+        self.model = model
+        self.fname += 'trades.csv' if self.model is None else '%s.csv'%self.model.name
+
+        columns = ['t','px','sz','sd','bid','mid','ask']
+        if self.model is not None: columns += [self.model.name]
+        self.df = pd.DataFrame(columns=columns)
+        self.initialized = False
+
+    def add(self, oid, side, price, size):
+        pass
+
+    def change(self, oid, side, newsize):
+        pass
+
+    def match(self, oid, side, price, size):
+        if not self.initialized:
+            if self.book.get_mid() is not None:
+                self.initialized = True
+                self.model.setup(self.book.get_mid())
+            else: return
+
+        side = 'buy' if side == 'sell' else 'sell'
+        bid = self.book.get_best_bid()
+        ask = self.book.get_best_ask()
+        mid = self.book.get_mid()
+
+        entry = {'t':int(datetime.today().strftime('%s')),
+                 'px':price, 'sz':size, 'sd':side[0],
+                 'bid':bid, 'mid':mid, 'ask':ask}
+
+        fmtsize = "-%0.4f"%size if side == "sell" else "+%0.4f"%size
+        if self.model is not None:
+            model_out = self.model.out(side, price, size)
+            entry[self.model.name] = model_out
+            pprint('%s (%0.2f), %0.2f/%0.2f/%0.2f => %0.2f'%(fmtsize, price, bid, mid, ask, model_out))
+        else:
+            pprint('%s (%0.2f), %0.2f/%0.2f/%0.2f'%(fmtsize, price, bid, mid, ask))
+        self.df = self.df.append(entry, ignore_index=True)
+        self.df.to_csv(self.fname)
+
+    def done(self, oid):
+        pass
+
+
+if __name__ == '__main__':
+    log.startLogging(sys.stdout)
+    factory = WebSocketClientFactory('wss://ws-feed.exchange.coinbase.com')
+    factory.protocol = BlobProtocol
+
+    model = None
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == 'glosten':
+            xk = (float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]))
+            gm = Glosten(xk)
+            model = gm
+
+    tl = TradeLogger(model=model)
+    bb = Book(factory.protocol, debug=False)
+    bb.add_client(tl)
+
+    connectWS(factory)
+    reactor.run()
