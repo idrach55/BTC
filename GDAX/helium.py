09b6315758acdebc250b7f30fbed7bf1c6a04139
idrach55 <isaac@drachman.org>
2016-10-08 17:36:42 -0400

    Updates

--- GDAX/helium.py
+++ GDAX/helium.py
@@ -123,9 +123,18 @@ if __name__ == '__main__':
     factory = WebSocketClientFactory('wss://ws-feed.exchange.coinbase.com')
     factory.protocol = BlobProtocol
 
-    # Setup params from params.py.
-    params_file = sys.argv[1]
-    exec(compile(open(params_file).read(), params_file, 'exec'))
+    # Setup params.
+    params = {
+        'debug'             : True,
+        'dump_on_lockdown'  : False,
+        'max_distance'      : 0.75,
+        'spread'            : 0.30,
+        'trade_size'        : 0.10,
+        'stop_loss'         : 0.30,
+        'max_inactive_time' : 600,
+        "shading"           : 0.95,
+        'speak'             : True
+    }
 
     if len(sys.argv) > 2:
         params["spread"] = float(sys.argv[2])
