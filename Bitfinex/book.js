4fc4e53e4f86bd093b3028d5632582429ca4af18
Isaac <isaac@drachman.org>
2016-11-26 17:21:34 -0500

    Update book.js

--- Bitfinex/book.js
+++ Bitfinex/book.js
@@ -1,12 +1,36 @@
 var BitfinexWS = require ('bitfinex-api-node').WS;
 var client = new BitfinexWS();
 
+var books = {'BTCUSD': {}, 'LTCUSD': {}, 'LTCBTC': {}};
+
+function getBestQuote(book, type) {
+    var levels = Object.keys(book).sort();
+    if (type == 'ask') levels.reverse();
+    var quote = {};
+    for (i = 0; i < levels.length; i++) {
+        if (type == 'bid') {
+            if (book[levels[i]] >= 0) quote = {'price': levels[i], 'size': book[levels[i]]};
+            if (book[levels[i]] < 0) break;
+        } else if (type == 'ask') {
+            if (book[levels[i]] <= 0) quote = {'price': levels[i], 'size': book[levels[i]]};
+            if (book[levels[i]] > 0) break;
+        }
+    }
+    return quote;
+}
+
 client.on('open', function () {
     client.subscribeOrderBook('BTCUSD');
+    client.subscribeOrderBook('LTCUSD');
+    client.subscribeOrderBook('LTCBTC');
 });
 
-client.on('orderbook', function (pair, book) {
-    console.log('Order book:', book);
+client.on('orderbook', function (pair, level) {
+    //console.log('Order book:', level);
+    books[pair][level['price']] = level['amount'];
+    bestBid = getBestQuote(books[pair], 'bid');
+    bestAsk = getBestQuote(books[pair], 'ask');
+    console.log(pair+': '+bestBid['price']+' | '+bestAsk['price']);
 });
 
 client.on('subscribed', function (data) {
@@ -14,3 +38,4 @@ client.on('subscribed', function (data) {
 });
 
 client.on('error', console.error);
+
